cc7426e38373917e99cb17c6a74a8a91eb85f08e
luthfi.pratomo@telekom.de <luthfi.pratomo@telekom.de>
2023-09-19 09:55:31 +0200

    quest 1

--- myProject/src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
+++ myProject/src/main/java/com/wildcodeschool/myProject/MyProjectApplication.java
@@ -2,7 +2,11 @@ package com.wildcodeschool.myProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class MyProjectApplication {
 
@@ -10,4 +14,9 @@ public class MyProjectApplication {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 
+	@RequestMapping("/greeting")
+	@ResponseBody
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
 }
